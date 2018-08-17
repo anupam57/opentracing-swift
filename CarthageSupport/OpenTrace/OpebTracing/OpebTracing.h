3783169ab98dedb126542d310b6ba2bca268983d
Anupam <anupam.iiita@gmail.com>
2018-08-17 13:01:50 +0200

    Adding support for Carthage

---
+++ CarthageSupport/OpenTrace/OpebTracing/OpebTracing.h
@@ -0,0 +1,19 @@
+//
+//  OpebTracing.h
+//  OpebTracing
+//
+//  Created by Anupam Choudhary on 17/08/18.
+//  Copyright © 2018 Zalando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpebTracing.
+FOUNDATION_EXPORT double OpebTracingVersionNumber;
+
+//! Project version string for OpebTracing.
+FOUNDATION_EXPORT const unsigned char OpebTracingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpebTracing/PublicHeader.h>
+
+
---
+++ CarthageSupport/OpenTrace/OpenTracingAPI/OpenTracingAPI.h
@@ -0,0 +1,19 @@
+//
+//  OpenTracingAPI.h
+//  OpenTracingAPI
+//
+//  Created by Anupam Choudhary on 17/08/18.
+//  Copyright © 2018 Zalando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenTracingAPI.
+FOUNDATION_EXPORT double OpenTracingAPIVersionNumber;
+
+//! Project version string for OpenTracingAPI.
+FOUNDATION_EXPORT const unsigned char OpenTracingAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenTracingAPI/PublicHeader.h>
+
+
